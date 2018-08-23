8a5640c93ba8f1728f9f13c8307bf61b6753475f
Jennifer Lawson <jenniferlawson000@gmail.com>
2018-08-23 10:54:21 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dictionaries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> showroom = new HashSet<string>();
+                showroom.Add("Jeep");
+                showroom.Add("Honda");
+                showroom.Add("Toyota");
+                showroom.Add("Mazda");
+
+
+                showroom.Add("Jeep");
+
+            HashSet<string> usedLot = new HashSet<string>();
+                usedLot.Add("Truck");
+                usedLot.Add("SUV");
+                showroom.UnionWith(usedLot);
+                showroom.Remove("SUV");
+            foreach (var car in showroom)
+            {
+                Console.WriteLine(car);
+            }
+        }
+    }
+}
